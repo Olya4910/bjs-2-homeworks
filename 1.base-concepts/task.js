5041b26020ea121eec9c966c05a4425753ac83e2
Olya Sem <olya4910@gmail.com>
2023-01-26 19:10:45 +0300

    Изменения внесены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,17 +3,15 @@ function solveEquation(a, b, c) {
   let discriminant
   let arr = [];
   discriminant = b ** 2 - 4 * a *c;
-  if (discriminant < 0) {
-    arr = [];
-  } else if (discriminant == 0) {
-    arr = -b / ( 2 * a );
+  if (discriminant == 0) {
+    arr.push(-b / ( 2 * a ));
   } else if (discriminant > 0) {
-    arr[0] = (-b + Math.sqrt(discriminant) )/(2*a);
-    arr[1] = (-b - Math.sqrt(discriminant) )/(2*a);
+    arr.push((-b + Math.sqrt(discriminant) )/(2*a));
+    arr.push((-b - Math.sqrt(discriminant) )/(2*a));
   }
   return arr;
 }
-console.log(solveEquation(15, 8, 9));
+console.log(solveEquation(1, 5, 4));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   if(isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
