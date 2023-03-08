502094c1e4b1d00b17c2f8a7b325a67ada0baad3
OLYA <105458754+Olya4910@users.noreply.github.com>
2023-03-08 21:17:46 +0300

    Add files via upload

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,62 @@
 ﻿
+function parseCount(argument) {
+    let result = Number.parseFloat(argument);
+        if (isNaN(result)) {
+        throw new Error("Невалидное значение");
+        } else {
+        return result; 
+        } 
+    }
+
+function validateCount(argument) {
+    
+    try{
+       return parseCount(argument);
+    } catch(Error){
+        return Error
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if ((a+b)< c || (a+c)< b || (b+c)< a) {
+            throw new MyError("Треугольник с такими сторонами не существует");
+        } 
+
+        this.a = a;
+        this.b = b;
+        this.c =c;
+
+    }
+    get perimeter() {
+        return this.a + this.b +this.c;
+    }
+    get area() {
+        const p = (this.a + this.b + this.c) / 2;
+        return +((p * (p - rhis.a) * (p - this.b) * (p - rhis.c)) ** 0.5).toFixed(3);
+        
+    }
+
+}
+
+class MyError extends Error {
+    constructor(text) {
+        super(text)
+        this._area = "Ошибка! Треугольник не существует";
+        this._perimeter = 'Ошибка! Треугольник не существует';
+    }
+    get area() {
+        return this._area;
+    }
+    get perimeter() {
+        return this._perimeter;
+    }
+}
+
+function getTriangle(a,b,c) {
+    try {
+        return new Triangle(a,b,c);
+    } catch(Error){
+        return Error;
+    }
+}
